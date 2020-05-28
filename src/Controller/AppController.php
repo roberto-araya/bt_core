80d1937d6d279bbab66386bdb676ed31ea5bbc8c
Roberto Araya <roberto.araya@btester.cl>
2020-05-28 13:45:27 -0400

    by roberto_araya: Add route to the default app dashboard.

---
+++ src/Controller/AppController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\bt_core\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * App dashboard controller.
+ */
+class AppController extends ControllerBase {
+
+  /**
+   * Returns a render-able array for a test page.
+   */
+  public function content() {
+    return [];
+  }
+
+}
